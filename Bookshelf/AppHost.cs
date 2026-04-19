f18f1970be7046a2b291dbb17808ccb9210130ed
Visual-Rock <visu.rock@proton.me>
2026-04-19 12:58:52 +0200

    add scalar open api viewer

--- Bookshelf/AppHost.cs
+++ Bookshelf/AppHost.cs
@@ -1,7 +1,8 @@
+using Aurora.AppHost.Extensions;
 using Projects;
 
 var builder = DistributedApplication.CreateBuilder(args);
 
-var api = builder.AddProject<Bookshelf_Api>("bookshelf-api");
+var api = builder.AddProject<Bookshelf_Api>("bookshelf-api").WithApiReference();
 
 builder.Build().Run();
---
+++ Bookshelf/Extensions/ResourceBuilderExtensions.cs
@@ -0,0 +1,18 @@
+﻿namespace Aurora.AppHost.Extensions;
+
+public static class ResourceBuilderExtensions
+{
+    public static IResourceBuilder<T> WithApiReference<T>(this IResourceBuilder<T> builder) where T : IResourceWithEndpoints
+    {
+        AddUrl(builder.GetEndpoint("http"), "Api (http)");
+        AddUrl(builder.GetEndpoint("https"), "Api (https)");
+
+        return builder;
+
+        void AddUrl(EndpointReference endPoint, string name)
+        {
+            if (endPoint.Exists)
+                builder.WithUrl($"{endPoint}/scalar/v1", name);
+        }
+    }
+}
