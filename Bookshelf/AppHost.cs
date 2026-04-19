0e633c5f328c9bbfe445c833fd03bff22a2e661c
Visual-Rock <visu.rock@proton.me>
2026-04-19 14:51:18 +0200

    add frontend

--- Bookshelf/AppHost.cs
+++ Bookshelf/AppHost.cs
@@ -1,3 +1,4 @@
+using Aspire.Hosting.JavaScript;
 using Aurora.AppHost.Extensions;
 using Bookshelf.Extensions;
 using Bookshelf.Helper;
@@ -11,6 +12,11 @@ var (_, db, dbMigrator) = builder.AddBookshelfDatabase();
 var api = builder.AddProject<Bookshelf_Api>("bookshelf-api")
     .WithApiReference()
     .WaitForCompletion(dbMigrator).WithReference(db);
+
+builder.AddViteApp("bookshelf-web", "../Bookshelf.Web")
+    .WithReference(api)
+    .WithExternalHttpEndpoints()
+    .PublishAsDockerFile();
 
 KeycloakHelper.ConfigureKeycloak(keycloak, api);
 KeycloakHelper.AddKeycloakEnvironment(keycloak, api);
