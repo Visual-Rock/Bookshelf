a14931de09b9223b9964965e1f6a6ba1e4113491
Visual-Rock <visu.rock@proton.me>
2026-04-29 07:44:45 +0200

    public libraries endpoint

---
+++ Bookshelf.Api/Controllers/LibraryController.cs
@@ -0,0 +1,19 @@
+﻿using Bookshelf.Api.Dto;
+using Bookshelf.Api.Extensions;
+using Bookshelf.Api.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Bookshelf.Api.Controllers;
+
+[ApiController]
+[Route("library")]
+public class LibraryController(IUserService userService, ILibraryService libraryService) : ControllerBase
+{
+    [HttpGet("public")]
+    public IActionResult GetPublicLibraries()
+    {
+        if (User.GetUser(userService) is not { } user)
+            return Unauthorized();
+        return Ok(libraryService.GetPublicLibraries().ToDtoList(user));
+    }
+}
